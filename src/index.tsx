d3e846926d9aac80ec6ee5cb40f180ef738203d5
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-04-25 20:35:08 -0400

    Update React to version 18.0.0

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ require("file-loader?name=[name].[ext]!html-minify-loader!./index.html");
 
 import App from "./components/app";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);
